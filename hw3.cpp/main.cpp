b2d6e29217d4c74f20cad2bc03f01566a2062092
Trang Vu <trangvu1109@yahoo.com>
2015-10-15 22:06:37 -0700

    Initial Commit

---
+++ hw3.cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hw3.cpp
+//
+//  Created by Trang Vu on 10/15/15.
+//  Copyright © 2015 Trang Vu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
